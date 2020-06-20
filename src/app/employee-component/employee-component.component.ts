5ec6b9ab9ef0e3c3c2eb833805a4cb84df5886c4
madushanka kumara <oska.coc@gmail.com>
2020-06-20 12:27:10 +0530

    Search Employee Done

--- src/app/employee-component/employee-component.component.ts
+++ src/app/employee-component/employee-component.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {error} from "util";
 
 interface EmployeeResponce {
-  status:string;
-  data:[{
+  status: string;
+  data: [{
     id: number
     employee_name: string
     employee_salary: number
@@ -22,37 +23,37 @@ interface EmployeeResponce {
 
 export class EmployeeComponentComponent implements OnInit {
   number;
-  save="Save";
-  tablelick:boolean;
+  save = "Save";
+  tablelick: boolean;
 
-employee = new FormGroup({
-    id:new FormControl("",Validators.required),
-    employee_name:new FormControl("",Validators.required),
-    employee_salary:new FormControl("",Validators.required),
-    employee_age:new FormControl("",Validators.required),
-    profile_image:new FormControl("")
+  employee = new FormGroup({
+    id: new FormControl("", Validators.required),
+    employee_name: new FormControl("", Validators.required),
+    employee_salary: new FormControl("", Validators.required),
+    employee_age: new FormControl("", Validators.required),
+    profile_image: new FormControl("")
   });
 
-  employees:any[];
-  private url='http://dummy.restapiexample.com/api/v1';
+  employees: any[];
+  private url = 'http://dummy.restapiexample.com/api/v1';
 
   constructor(
-    private route:ActivatedRoute,
-    private http:HttpClient) {
+    private route: ActivatedRoute,
+    private http: HttpClient) {
   }
 
   ngOnInit(): void {
     this.getcustomers();
   }
 
-  getcustomers(){
-    this.http.get<EmployeeResponce>(this.url+'/employees')
+  getcustomers() {
+    this.http.get<EmployeeResponce>(this.url + '/employees')
       .subscribe((responce) => {
-         this.employees= responce.data;
+        this.employees = responce.data;
       });
   }
 
-  btnDeleteClick($event,employee) {
+  btnDeleteClick($event, employee) {
     $event.stopPropagation();
     if (confirm("Do You Wish To Delete This Customer..!")) {
       this.http.delete(this.url + '/delete/' + employee.id)
@@ -67,18 +68,18 @@ employee = new FormGroup({
   }
 
   saveCustomer() {
-    if(this.tablelick){
-      this.http.put(this.url+'/update/'+ this.employee.value.id, this.employee.value)
+    if (this.tablelick) {
+      this.http.put(this.url + '/update/' + this.employee.value.id, this.employee.value)
         .subscribe(response => {
-          this.employees.splice(this.number,1,this.employee.value)
+          this.employees.splice(this.number, 1, this.employee.value)
           this.employee.reset();
-          this.tablelick=false;
-          this.save="save";
+          this.tablelick = false;
+          this.save = "save";
         });
 
-    }else {
+    } else {
       console.log(this.employee.value);
-      this.http.post(this.url+'/create',JSON.stringify(this.employee.value))
+      this.http.post(this.url + '/create', JSON.stringify(this.employee.value))
         .subscribe(response => {
           console.log(response);
           this.employees.push(this.employee.value);
@@ -88,27 +89,41 @@ employee = new FormGroup({
   }
 
   tableClick(employee) {
-    this.save="update"
-    this.tablelick=true;
+    this.save = "update"
+    this.tablelick = true;
     this.number = this.employees.indexOf(employee);
-    console.log(employee.name);
     this.employee.patchValue({
-      id:employee.id,
-      employee_name:employee.employee_name,
-      employee_salary:employee.employee_salary,
-      employee_age:employee.employee_age,
+      id: employee.id,
+      employee_name: employee.employee_name,
+      employee_salary: employee.employee_salary,
+      employee_age: employee.employee_age,
+      profile_image: employee.profile_image
     });
   }
 
   clearClick() {
-    this.save="save"
-    this.tablelick=false;
+    this.save = "save"
+    this.tablelick = false;
   }
 
-  searchEmployee(search: HTMLInputElement) {
-    this.http.get<EmployeeResponce>(this.url+'/employee/'+search.value)
-      .subscribe((responce) => {
-        this.employees= responce.data;
-      });
+  searchEmployee(search: any) {
+    if (search.target.value.length == 0) {
+      this.getcustomers();
+    } else {
+      if (search.key=="Enter") {
+        this.http.get<EmployeeResponce>(this.url + '/employee/' + search.target.value)
+          .subscribe((responce) => {
+              this.employees.splice(0, this.employees.length);
+              this.employees = responce.data;
+            },
+            (error) => {
+            },
+            () => {
+
+            });
+      }
+
+    }
   }
+
 }
